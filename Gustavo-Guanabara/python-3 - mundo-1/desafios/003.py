6b2c58636aacd1f0b062f78dca367169309f4f87
M7theus <m7theus_carvalho@protonmail.com>
2022-04-11 12:49:36 -0400

    .

--- Gustavo-Guanabara/python-3 - mundo-1/desafios/003.py
+++ Gustavo-Guanabara/python-3 - mundo-1/desafios/003.py
@@ -1,4 +1,4 @@
-n1 = input('Digite um número: ')
-n2 = input('Digite outro número: ')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
 soma = n1+n2
 print('A soma entre eles é:', soma)
