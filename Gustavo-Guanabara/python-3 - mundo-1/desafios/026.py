6f9821b76fe9eff6ff3e6c6d240c312281007af3
M7theus <m7theus_carvalho@protonmail.com>
2022-04-23 10:18:20 -0400

    Finalizando
    Conclusão dos desafios em puython propostos na aula 09 sobre manipulação de textos.

--- Gustavo-Guanabara/python-3 - mundo-1/desafios/026.py
+++ Gustavo-Guanabara/python-3 - mundo-1/desafios/026.py
@@ -1,14 +1,19 @@
+from itertools import count
+
+
 n1 = str(input('Digite uma frase qualquer: ')).strip()
-n10 = n1.lower()
+
 n2 = n1.count('A')
 n3 = n1.count('a')
-n4 = (len(n1) - n1.count(' '))
+
 a = n1.find('A')
-b = n1.find('a')
-c = n3 + n2
-d = n10.count('a')
+b = n1.find('a') 
+d = (n1.rfind('a') - n1.count(' ') - n1.count(','))
+
 print('Sua frase possue um total de {} A'.format(n2))
 print('Sua frase possuem um total de {} a'.format(n3))
+
 print('O primeiro *A* aparece na posição {}'.format(a))
 print('O *a* aparece na posição {}'.format(b))
+
 print('Última posição que aparece a letra *a* {}'.format(d))
---
+++ Gustavo-Guanabara/python-3 - mundo-1/desafios/027.py
@@ -0,0 +1,5 @@
+n1 = str(input('Digite seu nome completo: ')).strip()
+n2 = n1.split()
+
+print('A primeira parte do nome é {}'.format(n2[0]))
+print('A última parte do nome é {}'.format(n2[-1]))
