04a4c82177cb6d0d63997f07e2f2d15c7651c0ce
M7theus <m7theus_carvalho@protonmail.com>
2022-04-24 15:28:43 -0400

    Adicionando cores

---
+++ Gustavo-Guanabara/python-3 - mundo-1/exercicios/extra01.py
@@ -0,0 +1,8 @@
+print('\033[7;35;40mOlá mundo\033[m')
+print('\033[4;34;42mOlá mundo\033[m')
+print('\033[4;31mOlá mundo\033[m')
+print('\033[1;33mOlá mundo\033[m')
+print('Os valores são \033[31m3\033[m e \033[32m5\033[m')
+
+nome = 'Matheus'
+print('Olá {}{}{}'.format('\033[1;32m', nome, '\033[m'))
