c1dcf9f2bdec0817f472789c60ad1e247a0129f5
M7theus <m7theus_carvalho@protonmail.com>
2022-04-26 21:35:56 -0400

    045
    Finalizando o desafio 045 e conseguindo concluir todos os propostos na aula 10.

--- Gustavo-Guanabara/python-3-mundo-2/desafio/045.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/045.py
@@ -4,9 +4,22 @@ from emoji import emojize
 import emoji
 print(emoji.emojize('Que tal um jogo ?:video_game:'))
 print('Pedra, Papel ou Tesoura')
-a = str(input('Pronto? '))
 
 opcao = ['Pedra','Papel','Tesoura']
-print('Hmmm... Escolhendo o meu aqui')
-sleep(2)
+escolha = choice(opcao)
 n1 = str(input('Escolha a sua: '))
+if n1 == escolha:
+    print('Empatou')
+elif n1 == 'Pedra' and escolha == 'Tesoura':
+    print('Parabéns, vocễ venceu')
+elif n1 == 'Pedra' and escolha == 'Papel':
+    print('Eu ganhei :0')
+elif n1 == 'Papel' and escolha == 'Pedra':
+    print('Você venceu')
+elif n1 == 'Papel' and escolha == 'Tesoura':
+    print('Eu ganhei :0')
+elif n1 == 'Tesoura' and escolha == 'Papel':
+    print('Você ganhou')
+elif n1 == 'Tesoura' and escolha == 'Pedra':
+    print('Eu ganhei :0')
+print(escolha)
