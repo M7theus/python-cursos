ed06b9368515106e04e126df88152d22f44ba64d
M7theus <m7theus_carvalho@protonmail.com>
2022-04-27 21:25:27 -0400

    Corrigindo
    Realizando as correções de alguns exercícios

--- Gustavo-Guanabara/python-3-mundo-2/desafio/036.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/036.py
@@ -1,17 +1,8 @@
-from math import ceil
-
-print('\033[1;35m-=-\033[m'*20)
-print('Empréstimo casa')
-print('\033[1;35m-=-\033[m'*20)
-
-n1 = str(input('Digite seu nome completo porfavor: ')).strip().title()
-n2 = float(input('Digite o valor total em reais da casa R$:'))
-n3 = float(input('Digite sua renda mensal em reais R$:'))
-n4 = float(input('Digite em quantos anos deseja quitar a casa:'))
-mes = n4 *12
-prestacao = (n2 / mes) * 100 #Não pode exceder 30% do salário
-arre = ceil(prestacao)
-if prestacao > ((30/100)*n3):
-    print('Suas prestações vão ficar no valor \033[4;32mde R${}\033[m mensais. Seu empréstimo foi \033[1;31mcancelado\033[m'.format(arre))
+casa = float(input('Digite o valor da casa R$: '))
+salario = float(input('Digite o valor do seu salário mensal: '))
+ano = int(input('Digite a quantidade de anos: '))
+prestacao = casa / (ano *12)
+if prestacao >= (30/100) * salario:
+    print('Empréstimo negado')
 else:
-    print('Certo {}, seu empréstimo foi \033[1;32mliberado\033[m com sucesso. Suas prestações mensais serão no valor de R$:{}'.format(n1,arre))
+    print('Empréstimo concedido')
--- Gustavo-Guanabara/python-3-mundo-2/desafio/037.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/037.py
@@ -1,8 +1,10 @@
 n1 = int(input('Digite qualquer número inteiro: '))
-print('Digite\n\033[4;32m[ 1 ]\033[m para conversão \033[1;35mbinária\033[m\n\033[4;33m[ 2 ]\033[m para \033[1;36moctal\033[m\n\033[4;34m[ 3 ]\033[m para \033[1;37mhexadecimal\033[m: ')
-if n1 == 1:
-    print(bin(n1))
-#elif n1 == 2:
-    print()
-#else n1 == 3:
-    print()
+n2 = int(input('Digite\n\033[4;32m[ 1 ]\033[m para conversão \033[1;35mbinária\033[m\n\033[4;33m[ 2 ]\033[m para \033[1;36moctal\033[m\n\033[4;34m[ 3 ]\033[m para \033[1;37mhexadecimal\033[m: '))
+if n2 == 1:
+    print('{}'.format(bin(n1)[2:]))
+elif n2 == 2:
+    print('{}'.format(oct(n1)[2:]))
+elif n2 == 3:
+    print('{}'.format(hex(n1)[2:]))
+else:
+    print('Opção inválida.Tente novamente')
