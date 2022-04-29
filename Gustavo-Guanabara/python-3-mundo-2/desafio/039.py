d8408ee77312fa4a45b08adc4ead5c4ae1b0a73f
M7theus <m7theus_carvalho@protonmail.com>
2022-04-28 22:02:13 -0400

    Corrigindo
    Realizando a correção de alguns exercícios

--- Gustavo-Guanabara/python-3-mundo-2/desafio/039.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/039.py
@@ -1,11 +1,20 @@
 from datetime import date
-n1 = int(input('Digite o ano de nascimento: '))
-n2 = date.today().year
-if n2 - n1 < 18:
-    print('Falta {} anos para você ter que se alistar'.format(18-(n2 - n1)))
-elif n2 - n1 == 18:
-    print('Você possui 18 anos e está no tempo de se alistar')
-else:
-    print('Você já está {} anos atrasado para se alistar'.format(-(18-(n2 - n1))))
+data = date.today().year
+n1 = int(input('Digite seu ano de nascimento: '))
+n2 = str(input('Digite seu sexo [M/F]: ')).upper()
+idade = data - n1
 
-    
+if n2 == 2:
+    print('Você não precisa se alistar')
+elif idade < 18:
+    dado = 18 - idade + n1
+    falta = 18 - idade
+    print('Vocẽ ainda possui {} anos e portanto, não está no periodo de alistamento militar'.format(idade))
+    print('Falta {} anos para você se alistar'.format(falta))
+    print('Volte em {}'.format(dado))
+elif idade > 18:
+    dado = -idade + 18 + n1
+    print('Você possui {} anos e passou do tempo para se alistar'.format(idade))
+    print('Era para você ter se alistado em {}'.format(dado))
+else:
+    print('Você possue {} anos e está no tempo certo para realizar o alistamento'.format(idade))
