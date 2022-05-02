286a859878d0d93784e14a91086bae63707ce21b
M7theus <m7theus_carvalho@protonmail.com>
2022-05-01 20:44:44 -0400

    Corrigindo
    Finalizando as devidas correções dos exercícios

--- Gustavo-Guanabara/python-3-mundo-2/desafio/044.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/044.py
@@ -1,5 +1,12 @@
+print('{:=^40}'.format(' Loja '))
 n1 = float(input('Qual o preço do produto? '))
-n2 = int(input('Como será a forma de pagamento?\n [1.] À vista dinheiro/cheque: 10% de desconto.\n [2.] À vista no cartão: 5% de desconto.\n [3.] Em até 2x no cartão:preço normal.\n [4.] 3x ou mais no cartão: 20% de juros.\n [5.] Sair\n----> '))
+print('''Formas de pagamento:
+[ 1 ] À vista dinheiro/cartão
+[ 2 ] À Vista no cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão
+    ''')
+n2 = int(input('Qual será a forma de pagamento: '))
 if n2 == 1:
     print('Você escolheu a forma de pagamento à vista. Seu produto ficou no valor de R${}'.format(n1-(10/100)*n1))
 elif n2 == 2:
@@ -7,6 +14,8 @@ elif n2 == 2:
 elif n2 == 3:
     print('Você escolheu a forma de pagamento em até 2x no cartão. Seu produto ficou no valor de duas prestações de R${}'.format(n1/2))
 elif n2 == 4:
-    print('Você escolheu a forma de pagamento em 3x ou mais no cartão. Seu produto ficou no valor de R${}'.format(n1+(20/100)*n1))
-elif n2 ==5:
-    print('Programa finalizado')
+    qtaparcela = int(input('Quantas vezes no cartão ? '))
+    parcela = n1/qtaparcela
+    print('Você escolheu a forma de pagamento em {}x no cartão. Seu produto ficou no valor de R${:.2f}'.format(qtaparcela ,(n1+(20/100)*n1)/qtaparcela))
+else:
+    print('Opção inválida, tente novamente')
--- Gustavo-Guanabara/python-3-mundo-2/desafio/045.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/045.py
@@ -1,25 +1,36 @@
-from time import sleep
-from random import choice
-from emoji import emojize
-import emoji
-print(emoji.emojize('Que tal um jogo ?:video_game:'))
-print('Pedra, Papel ou Tesoura')
+from random import randint
+itens = ('Pedra' , 'Papel' , 'Tesoura')
+computador = randint(0,2)
+print('''
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Escolha sua opção de jogada: '))
+print('-=-' *10)
+print('O computador escolheu: \033[1;31m{}\033[m'.format(itens[computador]))
+print('O jogador escolheu: \033[1;33m{}\033[m'.format(itens[jogador]))
+print('-=-' *10)
 
-opcao = ['Pedra','Papel','Tesoura']
-escolha = choice(opcao)
-n1 = str(input('Escolha a sua: '))
-if n1 == escolha:
-    print('Empatou')
-elif n1 == 'Pedra' and escolha == 'Tesoura':
-    print('Parabéns, vocễ venceu')
-elif n1 == 'Pedra' and escolha == 'Papel':
-    print('Eu ganhei :0')
-elif n1 == 'Papel' and escolha == 'Pedra':
-    print('Você venceu')
-elif n1 == 'Papel' and escolha == 'Tesoura':
-    print('Eu ganhei :0')
-elif n1 == 'Tesoura' and escolha == 'Papel':
-    print('Você ganhou')
-elif n1 == 'Tesoura' and escolha == 'Pedra':
-    print('Eu ganhei :0')
-print(escolha)
+if jogador == 0: # Jogador escolheu pedra
+    if computador == 0:
+        print('Jogo \033[1;34mempatou\033[m')
+    elif computador == 1:
+        print('O computador \033[1;31mvenceu\033[m')
+    else:
+        print('O jogador \033[1;32mvenceu\033[m')
+    
+if jogador == 1: # Jogador escolheu Papel
+    if computador == 0:
+        print('O jogaodor \033[1;32mvenceu\033[m')
+    elif computador == 1:
+        print('O jogo \033[1;34mempatou\033[m')
+    else:
+        print('O computador \033[1;31mvenceu\033[m')
+    
+if jogador == 2: # Jogador escolheu Tesoura
+    if computador == 0:
+        print('O computador \033[1;31mvenceu\033[m')
+    elif computador == 1:
+        print('O jogador \033[1;32mvenceu\033[m')
+    else:
+        print('O jogo \033[1;34mempatou\033[m')
