a1f9f0a3c0207adf9744025626ff2340e5304098
M7theus <m7theus_carvalho@protonmail.com>
2022-05-13 20:56:57 -0400

    .

--- Gustavo-Guanabara/python-3-mundo-2/desafio/055.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/055.py
@@ -1,12 +1,9 @@
-t = 0
+s = 0
 
-
-for c in range(0,7):
-    peso = float(input('Digite seu peso: '))
-    t += peso
-    n1 = str(peso)    
-    n2 = n1.split()
-        
-print(n2)
-print(t)      
-print(c+1)
+for c in range(0,5):
+    peso = float(input('Digite seu peso em Kg: '))
+    s += peso
+    
+    
+    
+print(s)
