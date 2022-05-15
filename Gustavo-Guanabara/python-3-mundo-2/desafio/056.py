678bde65207a7a5e9f894de81b8ea45cd51f4f45
M7theus <m7theus_carvalho@protonmail.com>
2022-05-15 16:26:49 -0400

    Melhorando
    Melhorando o exercício

--- Gustavo-Guanabara/python-3-mundo-2/desafio/056.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/056.py
@@ -1,39 +1,47 @@
+from datetime import date
+
+data = date.today().year
+
+
 soma = 0
-
-mnome = 0
-midade = 0
-
-fidade = 0
-s = 0
+novo = 0
+pnome = 0
+vnome = 0
+velho = 0
+f = 0
 m = 0
 
-for rela in range(1,5):
-    print('-'*5, '{} Pessoa'.format(rela), '-'*5)
-    nome = str(input('Digite seu nome: ')).strip().title()
-    idade = float(input('Digite sua idade: '))
-    sexo = str(input('Sexo [M/F]: ')).upper().strip()
-    soma += idade
-    if rela == 1:
-        mnome = nome
-        midade = idade
-    else: 
-        if idade > midade:
-            midade = idade
-            mnome = nome   
-            
-    if rela == 1:
-        fidade = idade
+
+for tabela in range(1,5):
+    print('\033[1;31m-\033[m'*7,'\033[1;37m{}_Pessoa\033[m'.format(tabela),'\033[1;32m-\033[m'*7)
+    nome = str(input('Digite seu nome: ')).title().strip()
+    ano = int(input('Digite seu ano de nascimento: '))
+    sexo = str(input('Digite seu sexo [M/F]: ')).upper().strip()
+    idade = data - ano
+    
+    if tabela == 1:
+      novo = idade
+      pnome = nome
     else:
-        if sexo == 'M':
-            sexo = 'M'
-        elif fidade < 20:
-            s += 0
-        else:
-            m += 1
-
-media = soma / rela
-print('A média de idade do grupo é de {} anos'.format(media))   
-
-print('A pessoa mais velho tem {} anos e se chama {}'.format(midade,mnome)) 
-
-print('Ao todo são {} mulheres com mais de 20 anos'.format(m))
+        if novo > idade:
+            novo = idade
+            pnome = nome
+            
+    if tabela == 1:
+        vnome = nome       
+        velho = idade
+    else:
+        if velho < idade:
+            vnome = nome
+            velho = idade
+    
+    if sexo == 'M':
+        m += 1
+    if sexo == 'F':
+        f += 1    
+print(' ')
+media = idade / tabela
+print('A média das idades é: \033[1;36m{}\033[m'.format(media))
+print('A pessoa mais nova dentre os \033[1;37m{}\033[1;m é {} e possue {} anos'.format(tabela,pnome,novo))
+print('A pessoa mais velha dentre os \033[1;33m{}\033[m é {} e possue {} anos'.format(tabela,vnome,velho))
+print('Dessa lista de {} pessoas:\n--> \033[1;34m{}\033[m São homens\n--> \033[1;35m{}\033[m São mulheres'.format(tabela,m,f))
