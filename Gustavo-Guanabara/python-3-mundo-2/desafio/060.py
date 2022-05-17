5e847fb4ab343b0b90cedee05f5b9c6a0a8c1993
M7theus <m7theus_carvalho@protonmail.com>
2022-05-17 14:32:20 -0400

    Desafio.060
    Refazendo o desafio 60 utilizando o for e while

--- Gustavo-Guanabara/python-3-mundo-2/desafio/060.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/060.py
@@ -1,10 +1,18 @@
-numero = int(input('Digite um número qualquer para descobrir seu fatorial: '))
-soma = numero
-mult = 0
-a = 0
+# Com o larço while
 
-while a != numero:
-    mult = numero * soma
-    print('{}! = {}X{} = {}'.format(numero,soma,numero,mult),end = ' ')
-    soma -= 1
-    a += 1
+'''numero = int(input('Digite um número qualquer para descobrir seu fatorial: '))
+n = 1
+mult = numero
+
+while n != numero:
+    mult *= n
+    n += 1
+print('{}! = {}'.format(numero,mult))'''
+
+# Com o larço for
+
+numeros = int(input('Digite o número que deseja visualizar seu fatorial: '))
+mult = numeros
+for numero in range(1,numeros):
+    mult *= numero
+print('O fatorial do número {} é {}'.format(numeros,mult))
