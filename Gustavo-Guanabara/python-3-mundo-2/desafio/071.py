079e891616802f1272adf8ef77045ee6c5c32ed9
M7theus <m7theus_carvalho@protonmail.com>
2022-05-25 19:26:59 -0400

    Desafio_071_
    Corrigindo_o_desafio.

--- Gustavo-Guanabara/python-3-mundo-2/desafio/071.py
+++ Gustavo-Guanabara/python-3-mundo-2/desafio/071.py
@@ -1,49 +1,39 @@
-print('='*15)
-print('Banco - ')
-print('='*15)
-
-valor = int(input('Digite o valor a ser sacado: '))
-ced_50 = 50
-cont_50 = 0
-
-result_50 = valor
-
-ced_20 = 20
-cont_20 = 0
-
-ced_10 = 10
-cont_10 = 0
-
-ced_1 = 1
-cont_1 = 0
+valor = int(input('Digite o valor que queira sacar: '))
+total = valor
+cedula = 100
+cont = 0
 while True:
-    while result_50 >= 0:
-        print(f'{cont_50} 50')
-        cont_50 += 1
-        result_50 -= ced_50
-    if result_50 == 0:
-        break
+    if total >= 100:
+        total -= cedula
+        cont += 1
+        print(f'{cont} Notas de R$ 100')
     else:
-        sub_20 = result_50
-        while sub_20 >= 0:
-            print(f'{cont_20} 20')
-            cont_20 += 1
-            sub_20 -= ced_20
-        if sub_20 == 0:
+        cont = 0
+        if total >= 50:
+            cedula = 50
+            total -= cedula
+            cont += 1
+            print(f'{cont} Notas de R$ 50')
+        if total >= 20:
+            cedula = 20
+            total -= cedula
+            cont += 1
+            print(f'{cont} Notas de R$ 20')
+        if total >= 10:
+            cedula = 10
+            total -= cedula
+            cont += 1
+            print(f'{cont} Notas de R$ 10')
+        if total >= 5:
+            cedula = 5
+            total -= cedula
+            cont += 1
+            print(f'{cont} Notas de R$ 5')
+        if total >= 0:
+            cedula = 1
+            total -= cedula
+            cont += 1
+            print(f'{cont} Notas de R$ 1')
+        if total == 0:
             break
-        else:
-            sub_10 = sub_20
-            while sub_10 >= 0:
-                print(f'{cont_10} 10')
-                cont_10 += 1
-                sub_10 -= ced_10
-            if sub_10 == 0:
-                break
-            else:
-                sub_1 = sub_10
-                while sub_1 >= 0:
-                    print(f'{cont_1} 1')
-                    cont_1 += 1
-                    sub_1 -= ced_1
-                break
-
+            
