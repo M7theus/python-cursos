5cd627aa9687a8f2230c6c7b453d13f14e50942f
M7theus <m7theus_carvalho@protonmail.com>
2022-05-25 21:52:24 -0400

    072.

---
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/072.py
@@ -0,0 +1,11 @@
+nome = ('Zero','Um', 'Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quartoze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+
+pergunta = 22
+while pergunta > 20 or pergunta < 0:
+    pergunta = int(input('Digite um número entre 0 e 20: '))
+
+for c in range(0,len(pergunta)):
+    print(c)
+
+    
+    
