97d9e0e638644effadef40d0f7069b13e30bd209
M7theus <m7theus_carvalho@protonmail.com>
2022-05-28 16:42:43 -0400

    .

--- Gustavo-Guanabara/python-3-mundo-3/desafios/075.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/075.py
@@ -1,17 +1,13 @@
-valor_1 = int(input('Digite um número: '))
-valor_2 = int(input('Digite mais um: '))
-valor_3 = int(input('DIgite outro: '))
-valor_4 = int(input('Último número: '))
-
-lista = (valor_1,valor_2,valor_3,valor_4)
-print(lista)
-print(f'O número 9 apareceu: {lista.count(9)}')
-if lista == 3:
-    print(f'O número 3 foi digitado na posição {lista.index(3)+1}')
+termo = (int(input('Digite um número: ')),int(input('Digite outro número: ')), int(input('Digite mais um número: ')), int(input('Digite o último número: ')))
+print(f'\nOs valores digitados foram: {termo}')
+print(f'\nO valor 9 apareceu {termo.count(9)}')
+if 3 in termo:
+    print(f'\nO valor 3 foi digitado na posição: {termo.index(3)+1}')
 else:
-    print('O valor 3 não foi digitado em nenhuma posição')
-print(f'Os números pares foram: ')
+    print('\nO valor 3 não foi digitado nenhuma vez')
+print(f'\nOs numeros pares foram: ')
 
-for numeros in lista:
-    if numeros % 2 == 0:
-        print(numeros,end='')
+for numero in termo:
+    if numero % 2 == 0:
+        print(f'{numero}',end='')
+
--- Gustavo-Guanabara/python-3-mundo-3/desafios/076.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/076.py
@@ -1,7 +1,5 @@
-lista = ('Tomate', 3.40,'Banana',2.30,'Maça',4.59,'Manga',5.96,'Acerola',2.43)
-preco = 'LISTAGEM DE PREÇOS'
-print('-'*40)
-print(f'{preco:^40}')
-print('-'*40) 
-for termo,preco in lista:
-    print(f'{termo[0::1]:10} --> {preco[1::1]:10}')
+quantidade = int(input('Quantos produtos você deseja cadastrar no sistema: '))
+
+for produto in range(0,quantidade):
+    lista = (str(input('Escreva o nome do produto: ')), int(input('Preço do produto R$ ')))
+print(lista)
