94cf6bcbb646c2f3a4d342ec57ae743d9fc3bf3f
M7theus <m7theus_carvalho@protonmail.com>
2022-05-29 13:57:44 -0400

    Finalizando.
    Concluindo a correção dos desafios de python.

--- Gustavo-Guanabara/python-3-mundo-3/desafios/076.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/076.py
@@ -1,5 +1,12 @@
-quantidade = int(input('Quantos produtos você deseja cadastrar no sistema: '))
+from stringcolor import *
 
-for produto in range(0,quantidade):
-    lista = (str(input('Escreva o nome do produto: ')), int(input('Preço do produto R$ ')))
-print(lista)
+produtos = ('Lápis', 1.75, 'Borracha',2.21,'Apontador',3.89,'Caderno',22.94,'Corretivo',5.67,'Mochila',100.32,'Marcador',7.50)
+
+print('-='*30)
+print(f'{"Listagem de produtos":^60}')
+print('-='*30)
+for item in range(0,len(produtos)):
+    if item % 2 == 0:
+        print(cs(f'{produtos[item]:.<30}','#A63629',).bold(),end='')
+    else:
+        print(cs(f'{produtos[item]:>7.2f}','green').bold())
--- Gustavo-Guanabara/python-3-mundo-3/desafios/077.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/077.py
@@ -1,8 +1,9 @@
-palavras = ('Banana','Programador','Computador','Frase','Peixe','Casa','Pai','Mae','Irmao','Comida','Televisao',)
-consoantes = ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z',)
+palavras = ('Banana','Programador','Computador','Frase','Peixe','Casa','Pai','Mae','Irmao','Comida','Televisao','Python')
 
-for frases in palavras:
-    print(f'Na palavra {frases.upper()}')
-    
-for palavra in consoantes:
-    print((frases - palavra))
+for palavra in palavras:
+    print(f'\nPara {palavra.upper()} temos: ',end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(f'{letra}\n',end='')
+
+
