c2da71e6de8ec6c8b053ddbf8d283fbd7414d4c8
M7theus <m7theus_carvalho@protonmail.com>
2022-05-29 21:09:49 -0400

    .

--- Gustavo-Guanabara/python-3-mundo-3/desafios/078.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/078.py
@@ -1,11 +1,28 @@
 valores = list()
-for numero in range(0,5):
-    valores.append(int(input(f'Digite o {numero}: ')))
-print(f'Os números digitados foram: {valores}')
-print(f'O maior número entres os {numero} foi o {max(valores)}') 
-print(f'O menor número entre os {numero} foi o {min(valores)}')
-print(f'Os maiores valores encontram-se nas posições: ')
+for numeros in range(0,5):
+    valores.append(int(input(f'Digite o {numeros}: ')))
+print(f'Os valores digitados foram: {valores}')
+print(f'O maior número entre os {numeros} foi o {max(valores)} e suas posições são a {valores.index(max(valores))}')
+print(f'O menor número entre os {numeros} foi o {min(valores)} e suas posições são a {valores.index(min(valores))}')
 
-
-for n in valores:
-    print(max(valores))
+maior = max(valores)
+menor = min(valores)
+quant_maior = valores.count(maior)
+quant_menor = valores.count(menor)
+d = 0
+for valor in range(0,len(valores)):
+    print(f'{valor:.<10}',end='')
+    print(f'{valores[valor]:.>10}')
+    
+if maior in valores:
+    if quant_maior > 1:
+        for s in range(0,quant_maior):
+            print(valores.index(maior,d))
+            d += 1
+        d = 0
+    if menor > 1:
+        for s in range(0,quant_menor):
+            print(valores.index(menor,d))
+            d += 1
+        
+    
