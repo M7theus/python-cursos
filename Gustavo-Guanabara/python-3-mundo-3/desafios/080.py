4a7b4af724596d7cc444b0e53074eff480bcbd1d
M7theus <m7theus_carvalho@protonmail.com>
2022-06-01 21:24:09 -0400

    .

--- Gustavo-Guanabara/python-3-mundo-3/desafios/080.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/080.py
@@ -1,21 +1,29 @@
 lista = list()
-ant_numero = 0
+primero_numero = segundo_numero = 0
 for posicao in range(0,5):
-    numero = int(input('Digite um número: '))
+    numero = int(input('Digite seu número: '))
+    print(f'Posição: {posicao}')
     if posicao == 0:
-        lista.append(numero)
-        ant_numero = numero
-    else:
-        if numero > ant_numero:
-            print('Número adicionado ao final da lista')
+        primero_numero = numero
+        lista.insert(0, numero)
+    if posicao == 1:
+        if numero > primero_numero:
             lista.insert(posicao, numero)
-            ant_numero = numero 
-        elif numero < ant_numero:
-            print('Número adicionado ao início da lista')
-            if numero > lista[0] and numero < lista[1]:
-                lista.insert(1, numero)
+        else:
+            lista.insert(0, numero)
+    if posicao > 1:
+        if numero > primero_numero:
+            if numero > segundo_numero:
+                lista.insert(posicao, numero)
             else:
-                lista.insert(0,numero)
-            ant_numero = numero
-    print(lista)
+                lista.insert(posicao-1, numero)
+        if numero < primero_numero: 
+            if numero < segundo_numero:
+                lista.insert(0, numero)
+            else:
+                lista.insert(posicao-1, numero)
     
+    primero_numero = numero
+    segundo_numero = numero
+    print(lista)
+print(lista)
