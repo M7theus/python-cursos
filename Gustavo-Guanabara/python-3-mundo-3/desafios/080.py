2a34743ee02830ad50523f7d7fa7090a1b497d6e
M7theus <m7theus_carvalho@protonmail.com>
2022-06-04 15:03:02 -0400

    Correção.

--- Gustavo-Guanabara/python-3-mundo-3/desafios/080.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/080.py
@@ -1,8 +1,9 @@
 lista = []
 for posicao in range(0,5):
-    numero = int(input(f'Digite o número da {posicao} posição: '))
+    numero = int(input(f'Digite o {posicao} número: '))
     if posicao == 0 or numero > lista[-1]:
         lista.append(numero)
+        print('Número adicionado ao final da lista')
     else:
         pos = 0
         while pos < len(lista):
--- Gustavo-Guanabara/python-3-mundo-3/desafios/081.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/081.py
@@ -1,23 +1,17 @@
 lista = list()
-contagem = quan_cinco = 0
 while True:
-    numero = int(input('Digite um número: '))
-    if numero == 5:
-        quan_cinco += 1
-    contagem += 1
+    numero = int(input('Digite um valor: '))
     lista.append(numero)
-    lista.sort(reverse=True)
-    pergunta = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
-    if pergunta == 'N':
+    pergunta = str(input('Você deseja continuar? [S/N]: ')).strip()[0]
+    if pergunta in 'Nn':
         break
-print(f'Foram digitados um total de {contagem} números')
-print(f'A ordem decrescente dos números foi: {lista}')
-
+lista.sort(reverse=True)
+print(f'Foram digitados um total de {len(lista)}')
+print(f'A ordem decrescente da lista é: {lista}')
 if 5 in lista:
-    if quan_cinco > 1:
-        for posi in range(0,quan_cinco):
-            print(f'{lista.index(5,posi)+1}') 
-    else:
-        print(f'O número cinco foi digitado e se encontra na posição {lista.index(5)+1}')      
+    print(f'O cinco foi digitado e se encontra nas posições: ')
+    for p,v in enumerate(lista):
+        if v == 5:
+            print(p,end=' ')
 else:
-    print('O valor cinco não foi digitado')
+    print('O valor cinco não foi digitado na lista')
