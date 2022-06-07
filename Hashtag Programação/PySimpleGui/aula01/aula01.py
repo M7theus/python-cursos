c60b2a5f6fdda30eb5814d42b50c8b0a9a23455a
M7theus <m7theus_carvalho@protonmail.com>
2022-06-07 12:44:14 -0400

    Finalizando a aula.

--- Hashtag Programação/PySimpleGui/aula01/aula01.py
+++ Hashtag Programação/PySimpleGui/aula01/aula01.py
@@ -1,16 +1,21 @@
 import PySimpleGUI as sg
+from cotacao import pegar_cotacoes
 
 Layout = [
     [sg.Text("Pegar cotação da moeda")],
-    [sg.InputText()],
+    [sg.InputText(key="nome_cotacao")],
     [sg.Button("Pegar cotação"), sg.Button("Cancelar")],
-    [sg.Text("A cotação foi de tanto")],
+    [sg.Text("",key="texto_cotacao")],
 ]
 
-janela = sg.Window("Título da janela", Layout)
+janela = sg.Window("Sistema de cotação", Layout)
 
 while True:
     evento, valores = janela.read()
-    if evento == sg.WIN_CLOSED:
+    if evento == sg.WIN_CLOSED or evento == "Cancelar":
         break
+    if evento == "Pegar cotação":
+        codigo_cotacao = valores["nome_cotacao"]
+        cotacao = pegar_cotacoes(codigo_cotacao)
+        janela["texto_cotacao"].update(f"Pegando cotação {codigo_cotacao} é de R${cotacao}")
 janela.close()
