ea99ae55482b5b53209a18b7eceb72f3c1463131
M7theus <m7theus_carvalho@protonmail.com>
2022-06-07 21:47:10 -0400

    Update 084.py

--- Gustavo-Guanabara/python-3-mundo-3/desafios/084.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/084.py
@@ -1,29 +1,29 @@
-dados = list()
-total = list()
-quant_pessoa = 0
+reserva = list()
+principal = []
+maior = menor = 0
 while True:
-    dados.append(str(input('Digite seu nome: ')))
-    dados.append(int(input('Digite seu peso em (Kg): ')))
-    if quant_pessoa == 0:
-        maior = dados[1]
-        menor = dados[1]
+    reserva.append(str(input('Digite seu nome: ')).title())
+    reserva.append(float(input('Digite seu peso em (Kg): ')))
+    if len(principal) == 0:
+        maior = reserva[1]
+        menor = reserva[1]
     else:
-        if dados[1] > maior:
-            maior = dados[1]
-        elif dados[1] < menor:
-            menor = dados[1]
-    total.append(dados[:])
-    quant_pessoa += 1
-    dados.clear()
-    pergunta = str(input('Deseja continuar? [S/N]: ')).strip()[0]
+        if reserva[1] > maior:
+            maior = reserva[1]
+        elif reserva[1] < menor:
+            menor = reserva[1]
+    principal.append(reserva[:])
+    reserva.clear()
+    pergunta = str(input('Deseja continuar? [S/N] ')).strip()[0]
     if pergunta in 'Nn':
         break
-print(f'Foram cadasdrado um total de {quant_pessoa} pessoas')
-print(f'O maior peso é {maior}.0Kg. Peso de: ',end='')
-for p in total:
-    if p[1] == maior:
-        print(p[0])
-print(f'O menor peso é {menor}.0Kg. Peso de: ',end='')
-for p in total:
-    if p[1] == menor:
-        print(p[0])
+print('-='*30)
+print(f'Foram cadastrado um total de {len(principal)} de pessoas')
+print(f'O maior peso foi de {maior}Kg e pertence à: ',end='')
+for maio in principal:
+    if maio[1] == maior:
+        print(maio[0])
+print(f'O menor peso foi de {menor}Kg e pertence à: ',end='')
+for men in principal:
+    if men[1] == menor:
+        print(men[0])
