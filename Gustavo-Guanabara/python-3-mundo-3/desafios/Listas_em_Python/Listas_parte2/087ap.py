a57f55b820b432c68e425383c8d8d7ae7fc7256b
M7theus <m7theus_carvalho@protonmail.com>
2022-06-13 18:48:33 -0400

    Aprimoração do desafio 087.
    Finalmente conseguir resolver o problema de colunas e linhas. Agora é feito automaticamente o auto preenchimento.

--- Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
@@ -3,27 +3,38 @@ lista = list()
 coluna = int(input('Quantas colunas terá sua matriz? '))
 for _ in range(0,coluna):
     lista.append([])
+quantidade_coluna = coluna
 print(lista)
 
 while True:
-    linha = int(input('Quantas linhas terá sua coluna? '))
+    linha = int(input('Quantas linhas terá sua matriz? '))
     if linha % 2 == 1:
         print('Impossível formação de matriz. Tente novamente')
         print()
     else:
         break
+cont = 0
+teste = 0
 
-for linhas in range(0,linha):
+for linhas in range(0,coluna):
     if linhas % 2 == 0:
-        lista[linhas].append(0*coluna)
-    else:
-        lista[linhas].append(0)
+        while teste != linha:
+            lista[linhas].append(0)
+            teste += 1
+        teste = 0
+    elif linhas % 2 == 1:
+        while teste != linha:
+            lista[linhas].append(0)
+            teste += 1
+        teste = 0
+        
+    
 print(lista)
 
-for colunas in range(0,coluna):
+'''for colunas in range(0,coluna):
     for linhas in range(0,coluna):
         if linhas % 2 == 0:
             lista[colunas][linhas] = int(input(f'Digite o número da posição: [{colunas},{linhas}]: '))
         else:
             lista[colunas][linhas] = int(input(f'Digite o número da posição: [{colunas},{linhas}]: '))
-print(lista)
+print(lista)'''
