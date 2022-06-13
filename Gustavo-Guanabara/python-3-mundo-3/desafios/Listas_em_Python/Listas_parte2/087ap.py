67f52ff7f51e8074a3ae2dfd1b5294936275cc0c
M7theus <m7theus_carvalho@protonmail.com>
2022-06-13 12:35:45 -0400

    Update 087ap.py

--- Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
@@ -1,5 +1,5 @@
-colunas = []
-linhas = [[]]
+lista = [[],[]]
+dado = dict()
 
 print(f'{" Matriz ":=^30}')
 pergunta_colun = int(input('Quantas colunas terá sua matriz? '))
@@ -8,16 +8,14 @@ for coluna in range(0,pergunta_colun):
     for linha in range(0,pergunta_linha):
         pergunt = int(input(f'Digite [{coluna}, {linha}]: '))
         if linha == 0:
-            colunas.append(pergunt)
-        if linha  >= 1:
-            linhas[0].append(pergunt)
-print(colunas)
-print(linhas)
+            dado['coluna'] = pergunt
+            lista[0].append(dado.copy())
+        if linha >= 1:
+            dado['linha'] = pergunt
+            lista[1].append(dado.copy())
+        dado.clear()
+
 for coluna in range(0,pergunta_colun):
-    if coluna == 0:
-        print(f'{colunas}\n',end='')
-    for linha in range(0,pergunta_linha):
-        print(f'[{linhas[0][linha]}]',end='')
-        linhas.pop(0)
-        if linha == pergunta_colun-1:
-            print()
+    print(f'{lista[0][coluna]}')
+    for linha in range(0, pergunta_linha):
+        print(f'{lista[1][linha]}')
