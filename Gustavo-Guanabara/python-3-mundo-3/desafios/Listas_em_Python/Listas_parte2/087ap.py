b910330f206da142c97c4abb50d6a649d86aff5f
M7theus <m7theus_carvalho@protonmail.com>
2022-06-14 16:11:52 -0400

    Aprimoramento do desafio -
    Um simples aprimoramento no desafio 87. Agora você pode escolher as colunas e linhas que queira ter em sua matriz.

--- Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/Listas_em_Python/Listas_parte2/087ap.py
@@ -1,13 +1,19 @@
+from time import sleep
 lista = list()
 
 coluna = int(input('Quantas colunas terá sua matriz? '))
+print('Hmmm')
+sleep(0.5)
 for _ in range(0,coluna):
     lista.append([])
 quantidade_coluna = coluna
-print(lista)
+print()
+print(f'Suas colunas: {lista}')
 
 while True:
     linha = int(input('Quantas linhas terá sua matriz? '))
+    print('Hmmm')
+    sleep(0.5)
     if linha % 2 == 1:
         print('Impossível formação de matriz. Tente novamente')
         print()
@@ -27,14 +33,34 @@ for linhas in range(0,coluna):
             lista[linhas].append(0)
             teste += 1
         teste = 0
-        
-    
-print(lista)
+print()   
+print(f'Suas linhas: {lista}')
 
-'''for colunas in range(0,coluna):
-    for linhas in range(0,coluna):
-        if linhas % 2 == 0:
-            lista[colunas][linhas] = int(input(f'Digite o número da posição: [{colunas},{linhas}]: '))
+print('-=-' *30)
+print(f'Agora digite os valoes para a sua matriz: ')
+print('-=-'*30)
+for val_coluna in range(0,coluna):
+    for val_linha in range(0,linha):
+        numero = int(input((f'Digite o valor para a posição: [{val_coluna}, {val_linha}]: ')))
+        lista[val_coluna][val_linha] = numero
+
+cont = 0
+print(f'--> \033[1;32mPar\033[m')
+print(f'--> \033[1;31mÍmpar\033[m')
+print('-'*30)
+print('Matriz em formação: ')
+print()
+for val_coluna in range(0,coluna):
+    for val_linha in range(0,linha):
+        if cont == coluna:
+            cont = 0
+            print()
+        cont += 1
+        if lista[val_coluna][val_linha] % 2 == 0:
+            sleep(0.5)
+            print(f'\033[1;32m[{lista[val_coluna][val_linha]:^5}]\033[m',end='')
         else:
-            lista[colunas][linhas] = int(input(f'Digite o número da posição: [{colunas},{linhas}]: '))
-print(lista)'''
+            sleep(0.5)
+            print(f'\033[1;31m[{lista[val_coluna][val_linha]:^5}]\033[m',end='')
+
+print()
