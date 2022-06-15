6a96b2ad30950b821aed8cbefc7f50288c7b7994
M7theus <m7theus_carvalho@protonmail.com>
2022-06-15 19:00:09 -0400

    Teoria -
    Concluindo a parte de teoria sobre às funções em Python.

--- Gustavo-Guanabara/python-3-mundo-3/exercicios/Funções/015.py
+++ Gustavo-Guanabara/python-3-mundo-3/exercicios/Funções/015.py
@@ -5,11 +5,53 @@ linha()
 print('Teste')
 linha()'''
 
-def titulo(txt):
+'''def titulo(txt):
     print('-='*30)
     print(txt)
     print('-='*30)
 
 
 titulo('Curso em vídeo')
-titulo('Python')
+titulo('Python')'''
+
+'''def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma de A com B é: {s}')
+    
+    
+soma(b=4, a=5)'''
+
+'''def contador(*num):
+    for valor in num:
+        print(f'{valor} ',end='')
+    print('Fim')'''
+
+'''def contador(*num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} número')
+
+contador(2,1,7)
+contador(8,0)
+contador(2,3,4,5,6,7)'''
+
+'''def dobra(dobro):
+    pos = 0
+    while pos < len(dobro):
+        dobro[pos] *= 2
+        pos += 1
+
+
+valores = [9,4,3,2,1]
+dobra(valores)
+print(valores)'''
+
+def soma(*valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(s)
+
+
+soma(5,2)
+soma(2,9,4)
