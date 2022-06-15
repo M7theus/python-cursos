d93add166d21a8fc2c7b0fae99eca04855d74c0c
M7theus <m7theus_carvalho@protonmail.com>
2022-06-14 21:44:40 -0400

    Funções -
    Dando início ao conteúdo sobre funções.

---
+++ Gustavo-Guanabara/python-3-mundo-3/exercicios/Funções/015.py
@@ -0,0 +1,15 @@
+'''def linha():
+    print('-=-'*30)
+
+linha()
+print('Teste')
+linha()'''
+
+def titulo(txt):
+    print('-='*30)
+    print(txt)
+    print('-='*30)
+
+
+titulo('Curso em vídeo')
+titulo('Python')
