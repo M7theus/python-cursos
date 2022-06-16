1c7ee973bdb545579b25fd4a6fa61431dc895422
M7theus <m7theus_carvalho@protonmail.com>
2022-06-16 19:34:51 -0400

    Correção -
    Dando inicio a correção dos exercícios.

--- Gustavo-Guanabara/python-3-mundo-3/desafios/Funções/096.py
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/Funções/096.py
@@ -1,6 +1,6 @@
 def area (largura, comprimento):
     a = (largura * comprimento)
-    print(f'A área da {largura} largura com {comprimento} de comprimento é: {a:.1f}')
+    print(f'A área de um terreno {largura} x {comprimento} é de {a}m')
 
 largura = float(input('Digite a largura do terreno (m): '))
 comprimento = float(input('Digite o comprimento do terreno (m): '))
