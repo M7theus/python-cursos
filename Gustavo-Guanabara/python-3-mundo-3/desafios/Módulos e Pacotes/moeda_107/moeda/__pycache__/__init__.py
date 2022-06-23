fc7b736c457184a10a951962e14186606ee19de2
M7theus <m7theus_carvalho@protonmail.com>
2022-06-22 21:27:03 -0400

    Desafios -
    Dando início aos desafios.

---
+++ Gustavo-Guanabara/python-3-mundo-3/desafios/Módulos e Pacotes/moeda_107/moeda/__pycache__/__init__.py
@@ -0,0 +1,15 @@
+def metade(valor):
+    m = valor/2
+    return m
+
+
+def dobro(valor):
+    d = valor*2
+    return d
+
+
+def aumento (valor, porcentagem = 1):
+    co = (porcentagem/100)*valor + valor
+    
+def redução (valor, porcentagem =1):
+    re = (porcentagem/100)*valor - valor
